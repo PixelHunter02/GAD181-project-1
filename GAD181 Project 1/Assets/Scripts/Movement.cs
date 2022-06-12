d0fa2c6b30b7477e23635cec90a2d5e6b6444250
PixelHunter02 <107095317+PixelHunter02@users.noreply.github.com>
2022-06-12 21:51:47 +1000

    Test

--- GAD181 Project 1/Assets/Scripts/Movement.cs
+++ GAD181 Project 1/Assets/Scripts/Movement.cs
@@ -6,10 +6,9 @@ public class Movement : MonoBehaviour
 {
     CharacterController characterController;
 
-    public float speed;
-    public float mouseSpeed = 1;
-    public float gravity;
-    public float jumpSpeed;
+    public float speed; //speed set in inspector
+    public float gravity; // set gravity speed
+    public float jumpSpeed; // set jump speed in inspector
     private Vector3 moveDirection = Vector3.zero;
 
     // Start is called before the first frame update
@@ -21,22 +20,22 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (characterController.isGrounded)
-        {
+        // if (characterController.isGrounded)
+        // {
             // We are grounded, so recalculate
             // move direction directly from axes
 
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            moveDirection *= speed;
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        moveDirection *= speed;
 
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = jumpSpeed;
-            }
+        if (Input.GetButton("Jump"))
+        {
+            moveDirection.y = jumpSpeed;
         }
+        // }
         moveDirection.y -= gravity * Time.deltaTime;
         characterController.Move(moveDirection * Time.deltaTime);
 
-        transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Time.deltaTime * mouseSpeed);
+        // transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Time.deltaTime * mouseSpeed);
     }
 }
