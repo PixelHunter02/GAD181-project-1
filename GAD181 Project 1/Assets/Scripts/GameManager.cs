7a6749843bac03e499e3b66a546e0222f78b3a98
PixelHunter02 <107095317+PixelHunter02@users.noreply.github.com>
2022-06-15 03:35:06 +1000

    Update 2.0
    First Prototype
    Maze Created (Static)
    Animation
    Win Condition
    Lose Condition (Teleport)

--- GAD181 Project 1/Assets/Scripts/GameManager.cs
+++ GAD181 Project 1/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,11 +11,34 @@ public class GameManager : MonoBehaviour
     public float timer;
     public static bool interacting;
     public TextMeshProUGUI timerUI;
+    public GameObject[] spawn;
+    public float x;
+    public float y;
+    public float z;
+    public GameObject win;
+    public int random;
+    public CharacterController characterController;
+    public GameObject player;
     //public TextMeshProUGUI retry;
 
+    private void OnEnable() 
+    {
+        CollectiblePickUp.collectiblePickupEvent += Win;    
+
+    }
+
+    private void OnDisable() 
+    {
+        CollectiblePickUp.collectiblePickupEvent -= Win;    
+    }
     private void Start() 
     {
         timer = 10f;
+        random = Random.Range(0,6);
+
+        Instantiate(win, spawn[random].gameObject.transform.position, Quaternion.identity);
+        characterController = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -30,14 +54,22 @@ public class GameManager : MonoBehaviour
         {
             timerDoneEvent?.Invoke();
             timer = 10;
+            characterController.enabled = false;
+            player.transform.position = new Vector3(14.78125f, 3.814697e-06f, 33f);
             Time.timeScale = 0;
             //retry.text = "Press E To Try Again";
         }
 
         if(Input.GetKeyDown(KeyCode.E))
         {
+            characterController.enabled = true;
             Time.timeScale = 1;
             // retry.text = "";
         }
+    }
+    void Win()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+        SceneManager.LoadScene("GameOver");
     }
 }
